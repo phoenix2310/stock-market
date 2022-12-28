220f3088c88e579140193d82d5aeba0b16d3f018
phoenix2310 <121543343+phoenix2310@users.noreply.github.com>
2022-12-28 11:51:17 +0000

    database sync complete

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from dotenv import load_dotenv
-load_dotenv()
-from src.menu import showHeader,showMenu
-from src.stock import wantAdvice,sellStock
+from src.stock import wantAdvice, sellStock
+from src.menu import showHeader, showMenu
+from src.database import connect
 
+load_dotenv()
+
+connect()
 showHeader()
-ch=showMenu()
+ch = showMenu()
 print(ch)
-if ch==4:
+if ch == 4:
     wantAdvice()
-elif ch==2:
+elif ch == 2:
     sellStock()
 # elif c==1:
 #     return buyStock()
--- src/database.py
+++ src/database.py
@@ -1,13 +1,16 @@
 import os
 import MySQLdb
 
-connection = MySQLdb.connect(
-  host= os.getenv("HOST"),
-  user=os.getenv("USERNAME"),
-  passwd= os.getenv("PASSWORD"),
-  db= os.getenv("DATABASE"),
-  ssl_mode = "VERIFY_IDENTITY",
-  ssl      = {
-    "ca": "/etc/ssl/cert.pem"
-  }
-)
+connection=None
+def connect():
+    global connection
+    connection = MySQLdb.connect(
+        host=os.getenv("HOST"),
+        user=os.getenv("USERNAME"),
+        passwd=os.getenv("PASSWORD"),
+        db=os.getenv("DATABASE"),
+        ssl_mode="VERIFY_IDENTITY",
+        ssl={
+            "ca": "/etc/ssl/certs/ca-certificates.crt"
+        }
+    )
